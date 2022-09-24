c31caf906ab74f0d22f27b46c1e94b38a9105a3a
K-MHOON <azmria117@gmail.com>
2022-09-25 01:10:00 +0900

    feat: 로그아웃 버튼 활성화 기능 구현

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, call } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
@@ -8,14 +8,17 @@ import createRequestSaga, {
 const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그인 처리
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
   createRequestActionTypes('user/CHECK');
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 function checkFailureSaga() {
@@ -23,6 +26,15 @@ function checkFailureSaga() {
     localStorage.removeItem('user'); // localStorage에서 user를 제거한다.
   } catch (e) {
     console.log('localStorage is not working');
+  }
+}
+
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout); // logout API 호출
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
   }
 }
 
@@ -46,6 +58,10 @@ export default handleActions(
       ...state,
       user: null,
       checkError: error,
+    }),
+    [LOGOUT]: (state) => ({
+      ...state,
+      user: null,
     }),
   },
   initialState,
